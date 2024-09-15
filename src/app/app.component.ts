626128de848a01aa2727dc670e88b3c5c190f6f3
Denzil Jude Daniels <denziljudedaniels@gmail.com>
2024-09-15 16:42:16 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatGridListModule, MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'unstop';
+  tickets = [
+    {value: 1, viewValue: '1'},
+    {value: 2, viewValue: '2'},
+    {value: 3, viewValue: '3'},
+    {value: 4, viewValue: '4'},
+    {value: 5, viewValue: '5'},
+    {value: 6, viewValue: '6'},
+    {value: 7, viewValue: '7'},
+  ];
 }
